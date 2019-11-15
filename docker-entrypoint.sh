ecb119da78cac578c4cc24ad404234e2ee56dbbe
Eero Otsus <eero.otsus@pipedrive.com>
2019-11-15 23:48:14 +0200

    I like some contrast in my output...

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-echo "Running gustav-server"
+echo -e "\033[1mGUSTAV\033[0m Running gustav-server startup script..."
 ./gustav-server.sh &
 
-echo "Running the initial dockerd-entrypoint"
+echo -e "\033[1mGUSTAV\033[0m Running the initial dockerd-entrypoint..."
 ./usr/local/bin/dockerd-entrypoint.sh
--- gustav-server.sh
+++ gustav-server.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-echo "Waiting for Docker to spin up..."
+echo -e "\033[1mGUSTAV-SERVER\033[0m Waiting for Docker daemon to spin up..."
 until docker ps 2>/dev/null
 do
     sleep 1
 done
 
-echo "Docker is up..."
+echo -e "\033[1mGUSTAV-SERVER\033[0m  Docker daemon is up..."
 docker version
 
-echo "Spawning Gustav server..."
+echo -e "\033[1mGUSTAV-SERVER\033[0m Spawning Gustav server..."
 docker run --name gustav-manager -Pd node:12.13-alpine
 
 docker ps
